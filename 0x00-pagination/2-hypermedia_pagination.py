e5aebd23c83e78742cea787bb381492abab30b0d
Coderyounes <younesanas2021@gmail.com>
2024-05-03 09:38:07 +0100

    Task 2 : Hypermedia pagination Task was Fun

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+""" Documentation """
+
+import csv
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """
+        Calculate the start and end indices for a given page and page size.
+
+        Args:
+            page (int): The page number.
+            page_size (int): The number of items per page.
+
+        Returns:
+            tuple: A tuple containing the start and end indices.
+
+        """
+        start = (page - 1) * page_size
+        end = start + page_size
+        return (start, end)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Retrieves a specific page of data from the dataset.
+
+        Args:
+            page (int): The page number to retrieve (default is 1).
+            page_size (int): The number of items per page (default is 10).
+
+        Returns:
+            List[List]: The data for the specified page.
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        start, end = self.index_range(page, page_size)
+        assert page >= 0 and page_size > 0
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Retrieves a dictionary containing hypermedia pagination information.
+
+        Args:
+            page (int): The current page number. Defaults to 1.
+            page_size (int): The number of items per page. Defaults to 10.
+
+        Returns:
+            dict: A dictionary containing the following keys:
+                - 'page_size': The number of items per page.
+                - 'page': The current page number.
+                - 'data': The data for the current page.
+                - 'next_page': The next page number,
+                or None if there is no next page.
+                - 'prev_page': The previous page number,
+                or None if there is no previous page.
+                - 'total_pages': The total number of pages.
+        """
+
+        length = len(self.dataset())
+
+        def h(length, page_size):
+            t = length // page_size
+            if length % page_size != 0:
+                t += 1
+            return t
+
+        def n(curr, fn):
+            return curr < fn
+
+        def has_prev(curr):
+            return curr > 1
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if n(page, h(length, page_size)) else None,
+            'prev_page': page - 1 if has_prev(page) else None,
+            'total_pages': h(length, page_size)
+            }
