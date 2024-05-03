eec692f2c93c9fbd6c2c09463c8ab017c2557cd6
Coderyounes <younesanas2021@gmail.com>
2024-05-03 07:11:29 +0100

    Task0 : basic pagination calculation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 """ Documentation """
+
+
+def index_range(page: int, page_size:int ) -> tuple:
+    """
+    Calculate the start and end indices for a given page and page size.
+
+    Args:
+        page (int): The page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start and end indices.
+
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
