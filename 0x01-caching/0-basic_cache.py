81cf8b087899c3eb1776e6764597e60967ac0eaf
Coderyounes <younesanas2021@gmail.com>
2024-05-08 11:33:42 +0100

    Task0 : Basic Caching Class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Base Caching """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Args:
+            key: The key of the item.
+            item: The item to be added to the cache.
+        """
+        self.cache_data.update({key: item})
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache.
+
+        Args:
+            key: The key of the item to retrieve.
+
+        Returns:
+            The item associated with the given key,
+            or None if the key is not found in the cache.
+        """
+        return self.cache_data.get(key)
