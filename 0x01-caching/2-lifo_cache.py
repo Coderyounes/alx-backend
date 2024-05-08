b3ef3ea405bfad2f8610144f689d1f9697ca3fc8
Coderyounes <younesanas2021@gmail.com>
2024-05-08 15:02:17 +0100

    Task 2 : Last-in , first-out caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -6,4 +6,54 @@ from collections import OrderedDict
 
 
 class LIFOCache(BaseCaching):
-    pass
+    """
+    LIFOCache class represents a Last-In-First-Out (LIFO) caching mechanism.
+
+    Attributes:
+        cache_data (OrderedDict): A dictionary-like object
+        that maintains the order of insertion.
+
+    Methods:
+        put(key, item): Adds an item to
+        the cache with the specified key.
+        get(key): Retrieves the item associated with
+        the specified key from the cache.
+    """
+
+    def __init__(self):
+        """ Init Method """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache with the specified key.
+
+        Args:
+            key: The key to associate with the item.
+            item: The item to be added to the cache.
+
+        Returns:
+            None
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """
+        Retrieves the item associated with the specified key from the cache.
+
+        Args:
+            key: The key of the item to retrieve.
+
+        Returns:
+            The item associated with the specified key,
+            or None if the key is not found in the cache.
+        """
+        return self.cache_data.get(key)
