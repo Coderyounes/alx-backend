b2ed3d7ca819a7eb3cdc5892cea436b395069317
Coderyounes <younesanas2021@gmail.com>
2024-05-14 21:56:38 +0100

    Update shebang to use python3 in app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+""" Force locale with URL parameter """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -8,6 +8,14 @@ babel = Babel(app)
 
 
 class Config:
+    """
+    Configuration class for the application.
+
+    Attributes:
+        LANGUAGES (list): List of supported languages for the application.
+        BABEL_DEFAULT_LOCALE (str): Default locale for the application.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the application.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -16,13 +24,25 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
+    """
+    Get the best matching language for the user based on their request headers.
+
+    Returns:
+        str: The best matching language code.
+    """
+    lang = request.args.get('locale')
+    if lang in app.config['LANGUAGES']:
+        return lang
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def hello():
+    """
+    This function returns the rendered template '3-index.html'.
+    """
     return render_template('3-index.html')
 
 
