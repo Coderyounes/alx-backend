e4e34efae0f6e510b1d792d8276da0c29a6abfe2
Coderyounes <younesanas2021@gmail.com>
2024-06-12 22:07:05 +0100

    task 2:  Node Redis client and basic operations

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,6 +1,14 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient({
+  url: 'redis://127.0.0.1:6379',
+});
 
-client.on('connect', () => console.log('Redis client connected to the server'));
-client.on('error', (err) => console.log(`Redis client not connected to server: ${err}`));
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+client.connect().catch(console.error);
---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+  url: 'redis://127.0.0.1:6379',
+});
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => console.log(`Redis client not connected to server: ${err}`));
+
+const setNewSchool = async (schoolName, value) => {
+  const reply = await client.set(schoolName, value);
+  console.log('REPLY: ', reply);
+};
+
+const displaySchoolValue = async (schoolName) => {
+  const value = await client.get(schoolName);
+  console.log(value);
+};
+
+(async () => {
+  await client.connect().catch(console.error);
+  await displaySchoolValue('Holberton');
+  await setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
